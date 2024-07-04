bd4ca1090e992f58b3266062d0e7f55934cd59f7
Matheus Correa <matheus.barrosc@gmail.com>
2024-07-04 15:07:10 -0300

    Evolução da classe scraper.

--- scraper_code.py
+++ scraper_code.py
@@ -3,7 +3,23 @@ from bs4 import BeautifulSoup
 class Scraper():
     def __init__(self, content) -> None:
         self.content = content
+        self.soup = BeautifulSoup(self.content, 'html.parser')
 
-    def initiate_soup(self) -> BeautifulSoup:
-        soup = BeautifulSoup(self.content, 'html.parser')
-        return soup
+    def get_href_all(self, param, search_class) -> list:
+        all_links = self.soup.find_all(param, class_= search_class)
+        links = []
+        
+        for l in all_links:
+            links.append(l.get('href'))
+
+        return links
+    
+    def get_text_all(self, param, search_class) -> list:
+        all_response = self.soup.find_all(param, class_= search_class)
+        response_list = []
+        
+        for l in all_response:
+            response_list.append(l.get_text())
+
+        return response_list
+    

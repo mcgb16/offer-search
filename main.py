2f80744054917ae27426b3bab66dc3ff352e8640
Matheus Correa <matheus.barrosc@gmail.com>
2024-07-04 16:29:43 -0300

    Criação das buscas pelo nome dos produtos na kabum.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-import scraper_code
-import get_requests_code
+import sites
+import kabum
 
 if __name__ == "__main__":
-    pass
+    input_search = input('Digite o que deseja procurar: ')
+
+    kabum_search = kabum.get_kabum_search_url(input_search, page='1')
+    
+    kabum_html = sites.get_html(kabum_search)
+    kabum_soup = sites.create_scraper(kabum_html)
+
+    # Pegar a tag e a classe do componente que contém os nomes dos produtos.
+    kabum_product_name_tag, kabum_product_name_class = kabum.get_kabum_product_class_tag()
+
+    kabum_product_names_list = sites.get_products_name(kabum_soup, 
+    kabum_product_name_tag, kabum_product_name_class)
+
+    for i in kabum_product_names_list:
+        print(i)
+
+    # Pegar a tag e a classe do componente que contém 

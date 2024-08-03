c3fa25728245f86ad651b12f6d5b7abcaa710254
Matheus Correa <matheus.barrosc@gmail.com>
2024-08-02 21:00:13 -0300

    criação de arquivo que contém as informações presentes no html da pichau.

---
+++ sites/pichau.py
@@ -0,0 +1,21 @@
+pichau = {
+    'initial_url': 'https://www.pichau.com.br/',
+    'search': 'search?q=',
+    'page_number': '&page=',
+    'sort': '&sort='
+}
+
+pichau_html_specified = {
+    'product_name' : ('h2','MuiTypography-h6'),
+    'old_price' : ('', ''),
+    'current_price' : ('', ''),
+    'discount' : ('', ''),
+    'product_link' : ('', '')
+}
+
+def get_pichau_search_url(product, page) -> str:
+    search_url = pichau['initial_url'] + pichau['search'] + product
+    return search_url
+
+def get_pichau_product_class_tag() -> tuple:
+    return pichau_html_specified['product_name']

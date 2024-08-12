2396f4e7a2934b5b0315f9bdddaf341fd818caae
Matheus Correa <matheus.barrosc@gmail.com>
2024-08-12 16:47:29 -0300

    Criação de novos métodos para atender a busca por data-testid.

--- base_code/scraper_code.py
+++ base_code/scraper_code.py
@@ -14,6 +14,15 @@ class Scraper():
 
         return links
     
+    def get_href_all_data_testid(self, tag, search_testid) -> list:
+        all_links = self.soup.find_all(tag, attrs={"data-testid":search_testid})
+        links = []
+        
+        for l in all_links:
+            links.append(l.get('href'))
+
+        return links
+    
     def get_text_all(self, tag, search_class) -> list:
         all_response = self.soup.find_all(tag, class_= search_class)
         response_list = []
@@ -28,3 +37,16 @@ class Scraper():
         
         return all_response
     
+    def get_soup_all_data_testid(self, tag, search_testid) -> list:
+        all_response = self.soup.find_all(tag, attrs={"data-testid":search_testid})
+        
+        return all_response
+    
+    def get_text_all_data_testid(self, tag, search_testid) -> list:
+        all_response = self.soup.find_all(tag, attrs={"data-testid":search_testid})
+        response_list = []
+        
+        for l in all_response:
+            response_list.append(l.get_text())
+
+        return response_list

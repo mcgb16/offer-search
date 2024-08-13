7dca00cf66483a5e1c822d221518deeae29520c2
Matheus Correa <matheus.barrosc@gmail.com>
2024-08-13 15:42:38 -0300

    Anotação adicionada a respeito de peculiaridade encontrada no site da americanas.

--- base_code/get_requests_code.py
+++ base_code/get_requests_code.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
+# No site da americanas não é possível utilizar o headless
 chrome_options.add_argument('--headless')
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 

dd2a812326c08b60951e0bb9a51e7dd664c576a6
Matheus Correa <matheus.barrosc@gmail.com>
2024-08-15 17:04:57 -0300

    Inicio de organização dos códigos.

--- sites/kabum.py
+++ sites/kabum.py
@@ -35,3 +35,44 @@ def get_kabum_dicount_class_tag() -> tuple:
 
 def get_kabum_product_link_class_tag() -> tuple:
     return kabum_html_specified['product_link']
+
+    #KABUM
+
+    # kabum_search = kabum.get_kabum_search_url(input_search, page='1')
+
+    # kabum_html = get_requests_code.get_response(kabum_search)
+
+    # kabum_soup = scraper_code.Scraper(kabum_html)
+
+    # # Pegar a tag e a classe do componente que contém os nomes dos produtos.
+    # kabum_product_name_tag, kabum_product_name_class = kabum.get_kabum_product_class_tag()
+
+    # kabum_product_names_list = kabum_soup.get_text_all(kabum_product_name_tag, kabum_product_name_class)
+
+    # # Pegar a tag e a classe do componente que contém os preços.
+    # kabum_old_price_tag, kabum_old_price_class = kabum.get_kabum_old_price_class_tag()
+    # kabum_current_price_tag, kabum_current_price_class = kabum.get_kabum_current_price_class_tag()
+
+    # kabum_old_price_list = kabum_soup.get_text_all(kabum_old_price_tag, kabum_old_price_class)
+
+    # kabum_current_price_list = kabum_soup.get_text_all(kabum_current_price_tag, kabum_current_price_class)
+
+    # # Pegar a tag e a classe do componente que contém os descontos.
+
+    # kabum_discount_tag, kabum_discount_class = kabum.get_kabum_dicount_class_tag()
+
+    # kabum_discount_list = kabum_soup.get_text_all(kabum_discount_tag, kabum_discount_class)
+
+    # # Pegar a tag e a classe do componente que contém os links dos produtos.
+
+    # kabum_product_link_tag, kabum_product_link_class = kabum.get_kabum_product_link_class_tag()
+
+    # kabum_product_link_list = kabum_soup.get_href_all(kabum_product_link_tag, kabum_product_link_class)
+    # # Mesmo em situações que não tenha "old_price", ou seja, produtos que não estejam na promoção, este componente possui um valor, porém é um valor "vazio", por isso é possível sempre ter noção de quais são os valores e seus respectivos descontos sem um tratamento de dados mais detalhado.
+    # for i in range(10):
+    #     print(kabum_product_names_list[i])
+    #     if kabum_old_price_list[i] != '':
+    #         print(kabum_discount_list[i])
+    #         print(kabum_old_price_list[i])
+    #     print(kabum_current_price_list[i])
+    #     print(kabum_product_link_list[i])
--- sites/mercado_livre.py
+++ sites/mercado_livre.py
@@ -29,3 +29,41 @@ def get_mercadolivre_all_prices_class_tag() -> tuple:
 
 def get_mercadolivre_product_link_class_tag() -> tuple:
     return mercadolivre_html_specified['product_link']
+
+    # MERCADO LIVRE
+    
+    # mercadolivre_search = mercadolivre.get_mercadolivre_search_url(input_search, page='1')
+    # mercadolivre_html = get_requests_code.get_response(mercadolivre_search)
+
+    # mercadolivre_soup = scraper_code.Scraper(mercadolivre_html)
+    
+    # mercadolivre_product_name_tag, mercadolivre_product_name_class = mercadolivre.get_mercadolivre_product_class_tag()
+
+    # mercadolivre_product_names_list = mercadolivre_soup.get_text_all(mercadolivre_product_name_tag, mercadolivre_product_name_class)
+
+    # mercadolivre_old_price_tag, mercadolivre_old_price_class = mercadolivre.get_mercadolivre_old_price_class_tag()
+    # mercadolivre_current_price_tag, mercadolivre_current_price_class = mercadolivre.get_mercadolivre_current_price_class_tag()
+    # mercadolivre_all_prices_tag, mercadolivre_all_prices_class = mercadolivre.get_mercadolivre_all_prices_class_tag()
+
+    # mercadolivre_all_prices_list = mercadolivre_soup.get_soup_all(mercadolivre_all_prices_tag, mercadolivre_all_prices_class)
+    # mercadolivre_current_price_list = []
+    # mercadolivre_old_price_list = []
+
+    # for i in mercadolivre_all_prices_list:
+    #     old_price_item = i.find(mercadolivre_old_price_tag, attrs={"class":mercadolivre_old_price_class})
+    #     current_price_item = i.find(mercadolivre_current_price_tag, attrs={"class":mercadolivre_current_price_class})
+    #     if old_price_item:
+    #         mercadolivre_old_price_list.append(old_price_item.get_text())
+    #         mercadolivre_current_price_list.append(current_price_item.get_text())
+    #     else:
+    #         mercadolivre_old_price_list.append('')
+    #         mercadolivre_current_price_list.append(current_price_item.get_text())            
+
+    # mercadolivre_product_link_tag, mercadolivre_product_link_class = mercadolivre.get_mercadolivre_product_link_class_tag()
+    # mercadolivre_product_link_list = mercadolivre_soup.get_href_all(mercadolivre_product_link_tag, mercadolivre_product_link_class)
+
+    # for i in range (15):
+    #     print(mercadolivre_product_names_list[i])
+    #     print(mercadolivre_old_price_list[i])
+    #     print(mercadolivre_current_price_list[i])
+    #     print(mercadolivre_product_link_list[i])

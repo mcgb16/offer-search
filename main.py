999050026807dcdf48a44992bc6e5da303ea0989
Matheus Correa <matheus.barrosc@gmail.com>
2024-08-19 18:48:46 -0300

    Código ajustado para rodar o scraping na kabum após organização.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import base_code.scraper_code as scraper_code
-import base_code.get_requests_code as get_requests_code
 import sites.kabum as kabum
 import sites.pichau as pichau
 import sites.terabyte as terabyte
@@ -9,4 +7,19 @@ import sites.mercado_livre as mercadolivre
 
 if __name__ == "__main__":
     input_search = input('Digite o que deseja procurar: ')
-    
+
+    # KABUM   
+    kabum_soup = kabum.get_kabum_soup(input_search, "1")
+    kabum_product_names = kabum.get_kabum_product_names(kabum_soup)
+    kabum_old_prices = kabum.get_kabum_old_prices(kabum_soup)
+    kabum_current_prices = kabum.get_kabum_current_prices(kabum_soup)
+    kabum_discounts = kabum.get_kabum_discounts(kabum_soup)
+    kabum_product_links = kabum.get_kabum_product_links(kabum_soup)
+
+    for i in range(10):
+        print(kabum_product_names[i])
+        if kabum_old_prices[i] != '':
+            print(kabum_discounts[i])
+            print(kabum_old_prices[i])
+        print(kabum_current_prices[i])
+        print(kabum_product_links[i])

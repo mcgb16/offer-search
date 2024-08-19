aa93ae035e2722f713e9b66593bc406d23185648
Matheus Correa <matheus.barrosc@gmail.com>
2024-08-19 18:49:09 -0300

    Organização de todo o código referente ao scraping na kabum

--- sites/kabum.py
+++ sites/kabum.py
@@ -1,3 +1,6 @@
+import base_code.scraper_code as scraper_code
+import base_code.get_requests_code as get_requests_code
+
 kabum = {
     'initial_url': 'https://www.kabum.com.br/',
     'search': 'busca/',
@@ -9,70 +12,55 @@ kabum = {
 }
 
 kabum_html_specified = {
-    'product_name' : ('span','nameCard'),
-    'old_price' : ('span', 'oldPriceCard'),
-    'current_price' : ('span', 'priceCard'),
-    'discount' : ('div', 'discountTagCard'),
-    'product_link' : ('a', 'productLink')
+    'product_name' : ('span','nameCard','class'),
+    'old_price' : ('span', 'oldPriceCard','class'),
+    'current_price' : ('span', 'priceCard','class'),
+    'discount' : ('div', 'discountTagCard','class'),
+    'product_link' : ('a', 'productLink','class')
 }
 
-def get_kabum_search_url(product, page) -> str:
+def get_kabum_soup(product, page) -> scraper_code.Scraper:
     search_url = kabum['initial_url'] + kabum['search'] + product + kabum['initial_filter'] + kabum['page_size'] + kabum['page_number'] + page + kabum['sort'] + kabum['most_searched_sort']
     
-    return search_url
+    kbm_html = get_requests_code.get_response(search_url)
 
-def get_kabum_product_class_tag() -> tuple:
-    return kabum_html_specified['product_name']
+    kbm_soup = scraper_code.Scraper(kbm_html)
 
-def get_kabum_old_price_class_tag() -> tuple:
-    return kabum_html_specified['old_price']
+    return kbm_soup
 
-def get_kabum_current_price_class_tag() -> tuple:
-    return kabum_html_specified['current_price']
+def get_kabum_product_names(soup) -> tuple:
+    tag_name, class_name, search_type = kabum_html_specified['product_name']
 
-def get_kabum_dicount_class_tag() -> tuple:
-    return kabum_html_specified['discount']
+    product_names = soup.get_text_all(tag_name, class_name, search_type)
+    
+    return product_names
 
-def get_kabum_product_link_class_tag() -> tuple:
-    return kabum_html_specified['product_link']
+def get_kabum_old_prices(soup) -> tuple:
+    tag_name, class_name, search_type = kabum_html_specified['old_price']
 
-    #KABUM
+    kabum_old_prices = soup.get_text_all(tag_name, class_name, search_type)
+    
+    return kabum_old_prices
 
-    # kabum_search = kabum.get_kabum_search_url(input_search, page='1')
+def get_kabum_current_prices(soup) -> tuple:
+    tag_name, class_name, search_type = kabum_html_specified['current_price']
 
-    # kabum_html = get_requests_code.get_response(kabum_search)
+    kabum_current_prices = soup.get_text_all(tag_name, class_name, search_type)
+    
+    return kabum_current_prices
 
-    # kabum_soup = scraper_code.Scraper(kabum_html)
+def get_kabum_discounts(soup) -> tuple:
+    tag_name, class_name, search_type = kabum_html_specified['discount']
 
-    # # Pegar a tag e a classe do componente que contém os nomes dos produtos.
-    # kabum_product_name_tag, kabum_product_name_class = kabum.get_kabum_product_class_tag()
+    kabum_discounts = soup.get_text_all(tag_name, class_name, search_type)
+    
+    return kabum_discounts
 
-    # kabum_product_names_list = kabum_soup.get_text_all(kabum_product_name_tag, kabum_product_name_class)
+def get_kabum_product_links(soup) -> tuple:
+    tag_name, class_name, search_type = kabum_html_specified['product_link']
 
-    # # Pegar a tag e a classe do componente que contém os preços.
-    # kabum_old_price_tag, kabum_old_price_class = kabum.get_kabum_old_price_class_tag()
-    # kabum_current_price_tag, kabum_current_price_class = kabum.get_kabum_current_price_class_tag()
+    kabum_product_links = soup.get_href_all(tag_name, class_name, search_type)
+    
+    return kabum_product_links
 
-    # kabum_old_price_list = kabum_soup.get_text_all(kabum_old_price_tag, kabum_old_price_class)
-
-    # kabum_current_price_list = kabum_soup.get_text_all(kabum_current_price_tag, kabum_current_price_class)
-
-    # # Pegar a tag e a classe do componente que contém os descontos.
-
-    # kabum_discount_tag, kabum_discount_class = kabum.get_kabum_dicount_class_tag()
-
-    # kabum_discount_list = kabum_soup.get_text_all(kabum_discount_tag, kabum_discount_class)
-
-    # # Pegar a tag e a classe do componente que contém os links dos produtos.
-
-    # kabum_product_link_tag, kabum_product_link_class = kabum.get_kabum_product_link_class_tag()
-
-    # kabum_product_link_list = kabum_soup.get_href_all(kabum_product_link_tag, kabum_product_link_class)
-    # # Mesmo em situações que não tenha "old_price", ou seja, produtos que não estejam na promoção, este componente possui um valor, porém é um valor "vazio", por isso é possível sempre ter noção de quais são os valores e seus respectivos descontos sem um tratamento de dados mais detalhado.
-    # for i in range(10):
-    #     print(kabum_product_names_list[i])
-    #     if kabum_old_price_list[i] != '':
-    #         print(kabum_discount_list[i])
-    #         print(kabum_old_price_list[i])
-    #     print(kabum_current_price_list[i])
-    #     print(kabum_product_link_list[i])
+# Mesmo em situações que não tenha "old_price", ou seja, produtos que não estejam na promoção, este componente possui um valor, porém é um valor "vazio", por isso é possível sempre ter noção de quais são os valores e seus respectivos descontos sem um tratamento de dados mais detalhado.
